d7f28263d467636008376ee4caddbac2f6f50c71
abhinavyadav1806 <abhinav1806yadav@gmail.com>
2020-12-08 10:10:07 +0530

    ..

---
+++ web/login.php
@@ -0,0 +1,52 @@
+<!--
+Au<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+	<?php require('header.php'); ?>
+</head>
+<body>
+<!---login--->
+<div class="content">
+	<div class="main-1">
+		<div class="container">
+			<div class="login-page">
+				<div class="account_grid">
+					<div class="col-md-6 login-left">
+							<h3>new customers</h3>
+							<p>By creating an account with our store, you will be able to move through the checkout process faster, store multiple shipping addresses, view and track your orders in your account and more.</p>
+							<a class="acount-btn" href="account.html">Create an Account</a>
+					</div>
+					<div class="col-md-6 login-right">
+						<h3>registered</h3>
+						<p>If you have an account with us, please log in.</p>
+						<form>
+							<div>
+							<span>Email Address<label>*</label></span>
+							<input type="text"> 
+							</div>
+							<div>
+							<span>Password<label>*</label></span>
+							<input type="password"> 
+							</div>
+							<a class="forgot" href="#">Forgot Your Password?</a>
+							<input type="submit" value="Login">
+						</form>
+					</div>	
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- login -->
+<div>
+	<?php require('footer.php') ?>
+</div>
+</body>
+</html>

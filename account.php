dc9f56dedd93a0b0c2af79070fdf76e26cbce91c
abhinavyadav1806 <abhinav1806yadav@gmail.com>
2020-12-09 16:53:31 +0530

    .

--- account.php
+++ account.php
@@ -1,98 +1,101 @@
-<?php
-	include("header.php");
+<?php 
+    require_once("header.php"); 
 
-	include('class/Dbcon.php');
-	include('class/User.php');
+    require_once('class/Dbcon.php');
+    require_once('class/User.php');
 
-	// $Dbcon = new Dbcon();
-	// $User = new User();
+    $Dbcon = new Dbcon();
+    $User = new User(); 
 
-	// if(isset($_POST['submit']))
-    // {
-    //     $name = isset($_POST['name']) ? ($_POST['name']) : "";
-	// 	$email = isset($_POST['email']) ? ($_POST['email']) : "";
-	// 	$mobile = isset($_POST['mobile']) ? ($_POST['mobile']) : "";
-	// 	$password = isset($_POST['password']) ? ($_POST['password']) : "";
-	// 	$cnfm_password = isset($_POST['cnfm_password']) ? ($_POST['cnfm_password']) : "";
-	// 	$question = isset($_POST['question']) ? ($_POST['question']) : "";
-	// 	$answer = isset($_POST['answer']) ? ($_POST['answer']) : "";
+    if(isset($_POST['submit']))
+    {
+        $name = isset($_POST['name']) ? ($_POST['name']) : "";
+		$email = isset($_POST['email']) ? ($_POST['email']) : "";
+		$mobile = isset($_POST['mobile']) ? ($_POST['mobile']) : "";
+		$password = isset($_POST['password']) ? ($_POST['password']) : "";
+		$cnfm_password = isset($_POST['cnfm_password']) ? ($_POST['cnfm_password']) : "";
+		$question = isset($_POST['question']) ? ($_POST['question']) : "";
+		$answer = isset($_POST['answer']) ? ($_POST['answer']) : "";
 		
-    //     $sql = $User->Signup($name,$email,$mobile,$password,$cnfm_password,$question,$answer,$Dbcon->connect);
-    //     echo $sql;
-    // }
+        $sql = $User->Signup($name,$email,$mobile,$password,$cnfm_password,$question,$answer,$Dbcon->connect);
+        echo $sql;
+    }
 ?>
+
 
 <div class="content">
     <!-- registration -->
-	<div class="main-1">
-		<div class="container">
-			<div class="register">
-			
-		  	  	<form action="" method="POST"> 
-					<div class="register-top-grid">
-						<h3>personal information</h3>
-						<div>
-							<span>Name<label>*</label></span>
-							<input type="text" name="name" > 
-						</div>
+    <div class="main-1">
+        <div class="container">
+            <div class="register">
 
-						<div>
-							<span>Email<label>*</label></span>
-							<input type="email" name="email" > 
-						</div>
+                <form action="" method="POST">
+                    <div class="register-top-grid">
 
-						<div>
-							<span>Mobile<label>*</label></span>
-							<input type="text" name="mobile" maxlength="10" > 
-						</div>
+                        <h3>personal information</h3>
+                        <div>
+                            <span>Name<label>*</label></span>
+                            <input type="text" name="name" class="lugwt" required onkeydown="return alphaonly(event);">
+                        </div>
 
-						<div class="clearfix"> </div>
-						<a class="news-letter" href="#">
-							<!-- <label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i>Sign Up for Newsletter</label> -->
-						</a>
-						</div>
-						
-						<div class="register-bottom-grid">
-							<h3>login information</h3>
-							
-							<div>
-								<span>Password<label>*</label></span>
-								<input type="password" name="password" >
-							</div>
+                        <div>
+                            <span>Mobile<label>*</label></span>
+                            <input type="text" name="mobile" id="mobile" maxlength="10" class="lugwt" onkeydown="return onlynumber(event);" required>
+                        </div>
 
-							<div>
-								<span>Confirm Password<label>*</label></span>
-								<input type="password" name="cnfm_password" >
-							</div>
+                        <div>
+                            <span>Email<label>*</label></span>
+                            <input type="email" name="email" id="email" class="lugwt" onkeydown="return alphaonly3(event);" required >
+                        </div>
 
-							<div>
-								<span>Enter Security Question<label>*</label></span>
-								<input type="text" name="question" >
-							</div>
-							
-							<div>
-								<span>Enter Security Answer<label>*</label></span>
-								<input type="text" name="answer" >
-							</div>
-						</div>
-					</div>
-				</form>
+                        <div>
+                            <span>Pickup Security Question<label>*</label></span>
+                            <select class="question" name="question" required>
+                                <option selected disabled>Choose Security Question</option>
+                                <option value="What was your childhood nickname?">What was your childhood nickname?</option>
+                                <option value="What is the name of your favourite childhood friend?">What is the name of your favourite childhood friend?</option>
+                                <option value="What was your favourite place to visit as a child?">What was your favourite place to visit as a child?</option>
+                                <option value="What was your dream job as a child?">What was your dream job as a child?</option>
+                                <option value="What is your favourite teacher's nickname?">What is your favourite teacher's nickname?</option>
+                            </select>
+                        </div>
 
-				<div class="clearfix"></div>
+                        <div>
+                            <span>Answer<label>*</label></span>
+                            <input type="text" class="lugwt" name="answer" required pattern="^[a-zA-Z0-9]+$"
+                            onkeydown="return alphaonly2(event);">
+                        </div>
 
-				<div class="register-but">
-				   <form action="" method="POST">
-					   <input type="submit" value="Register" name="submit">
-					   <div class="clearfix"> </div>
-				   </form>
-				</div>
-		   </div>
-		 </div>
-	</div>  
+                        <div class="clearfix"></div>
+                        <a class="news-letter" href="#"></a>
+                    </div>
+
+                    <div class="register-bottom-grid">
+                        <h3>login information</h3>
+                        <div>
+                            <span>Password<label>*</label></span>
+                            <input type="password" name="password" class="lugwt" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" minlength="8" maxlength="16" required>
+                        </div>
+
+                        <div>
+                            <span>Confirm Password<label>*</label></span>
+                            <input type="password" name="cnfm_password" class="lugwt" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" minlength="8" maxlength="16" required>
+                        </div>
+
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <div class="register-but">
+                    <input type="submit" value="Register" name="submit" class="a">
+                    <div class="clearfix"> </div>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
 </div>
-<!-- login -->
-<?php
-    require("footer.php");
-?>
-</body>
-</html>
+<!-- registration -->
+</div>
+
+<?php require "footer.php" ?>
--- class/User.php
+++ class/User.php
@@ -1,45 +1,42 @@
 <?php
-    // class User
-    // {
-    //     function Signup($name,$email,$mobile,$password,$cnfm_password,$question,$answer,$connect)
-    //     {
-    //         if($password != $cnfm_password)
-    //         {
-    //             return 'Enter Same Password';
-    //         }
+    class User
+    {
+        function Signup($name,$email,$mobile,$password,$cnfm_password,$question,$answer,$connect)
+        {
+            if($password != $cnfm_password)
+            {
+                return 'Enter Same Password';
+            }
 
-    //         // $sql = "SELECT * FROM tbl_user";
-    //         // $result = mysqli_query($connect,$sql);
+            $sql = "SELECT * FROM tbl_user";
+            $result = mysqli_query($connect,$sql);
 
-    //         // if($result->num_rows >0)
-    //         // {
-    //         //     while($row=$result->fetch_assoc())
-    //         //     {
-    //         //         $_SESSION['user']=array('username'=>$row['user_name'], 'isblock'=>$row['is_block']);
+            if($result->num_rows >0)
+            {
+                while($row=$result->fetch_assoc())
+                {
+                    $_SESSION['user']=array('usermobile'=>$row['mobile']);
 
-    //         //         $trimed_username = trim($username);
-    //         //         $strtolower = strtolower($trimed_username);
-                    
-    //         //         if($_SESSION['user']['username'] == $strtolower)
-    //         //         {
-    //         //             return "Enter Unique User Name";
-    //         //         }
-    //         //     }
-    //         // }
+                    if($_SESSION['user']['usermobile'] == $mobile)
+                    {
+                        return("User Already Exist..!!");
+                    }
+                }
+            }
 
-    //         $insert = "INSERT INTO tbl_user(`email`, `name`, `mobile`, `email_approved`, `phone_approved`, `active`, `is_admin`, `sign_up_date`, `password`, `security_question`, `security_answer`) 
-    //         VALUES('$email', '$name', '$mobile', NOW(), '$mobile', 0, MD5('$password'), 0)";
+            $insert = "INSERT INTO tbl_user(`email`, `name`, `mobile`, `is_admin`, `sign_up_date`, `password`, `security_question`, `security_answer`) 
+            VALUES('$email', '$name', '$mobile', 0, NOW(), MD5('$password'), '$question', '$answer')";
 
-    //         if($connect ->query($insert) === TRUE)
-    //         {
-    //             // echo "New Record Added Successfully";
-    //             header("Location:login.php");
-    //         }
-    //         else
-    //         {
-    //             echo "failed";
-    //         }  
-    //         $connect->close();
-    //     }
-    // }
+            if($connect ->query($insert) === TRUE)
+            {
+                echo "New Record Added Successfully";
+                // header("Location:login.php");
+            }
+            else
+            {
+                echo "failed";
+            }  
+            $connect->close();
+        }
+    }
 ?>
